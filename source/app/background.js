18b6b091ed5f38288ff694d71912f1c05303da27
chren <peter.privalinec@gmail.com>
2015-11-11 07:20:06 +0100

    added error handler for background dropbox api

--- source/app/background.js
+++ source/app/background.js
@@ -1,5 +1,34 @@
 'use strict';
-var client = new Dropbox.Client({ key: "k1qq2saf035rn7c" }),
+var client = new Dropbox.Client({key: "k1qq2saf035rn7c"}),
+    handleError = function (error) {
+        switch (error.status) {
+            case Dropbox.ApiError.INVALID_TOKEN:
+                console.warn('user token expired ', error.status);
+                break;
+
+            case Dropbox.ApiError.NOT_FOUND:
+                console.warn('file or dir not found ', error.status);
+                break;
+
+            case Dropbox.ApiError.OVER_QUOTA:
+                console.warn('dropbox quota overreached ', error.status);
+                break;
+
+            case Dropbox.ApiError.RATE_LIMITED:
+                console.warn('too many API calls ', error.status);
+                break;
+
+            case Dropbox.ApiError.NETWORK_ERROR:
+                console.warn('network error, check connection ', error.status);
+                break;
+
+            case Dropbox.ApiError.INVALID_PARAM:
+            case Dropbox.ApiError.OAUTH_ERROR:
+            case Dropbox.ApiError.INVALID_METHOD:
+            default:
+                console.warn('network error, check connection ', error.status);
+        }
+    },
     tempStorage = {
         'tags': {
             '0': {
@@ -25,6 +54,9 @@ var client = new Dropbox.Client({ key: "k1qq2saf035rn7c" }),
         disconnected: {color: [237, 199, 85, 100], defaultText: '\u0020'},
         throttled: {color: [255, 255, 0, 100], defaultText: '!'}
     };
+
+client.authDriver(new Dropbox.AuthDriver.ChromeExtension({receiverPath: "html/chrome_oauth_receiver.html"}));
+
 
 function updateBadgeStatus(status) {
     chrome.browserAction.setBadgeText({text: badgeState[status].defaultText});
