5c38e8b72a43caa01b8a12683cd7eabef7181ab3
Pavol Rusnak <stick@gk2.sk>
2016-01-19 19:12:25 +0100

    preparations for release

---
+++ release.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cd source
+rm -f *.zip
+cp -a app/ extension/
+zip -r extension-newtab.zip extension/
+# remove new tab attribute
+sed -i 's/(with New Tab feature)/(without New Tab feature)/' extension/manifest.json
+sed -i '/chrome_url_overrides/,+2d' extension/manifest.json
+zip -r extension-no-newtab.zip extension/
+rm -rf extension/
+cd ..
