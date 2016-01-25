c87f2721d3b284614e5d5df815c318446765a083
chren <peter.privalinec@gmail.com>
2016-01-25 18:52:33 +0100

    added safenote to decrypt [fix]

--- source/app/dashboard/password_table/table_entry/table_entry.js
+++ source/app/dashboard/password_table/table_entry/table_entry.js
@@ -350,6 +350,7 @@ var React = require('react'),
                 title: this.state.title,
                 username: this.state.username,
                 password: this.state.password,
+                safe_note: this.state.safe_note,
                 nonce: this.state.nonce
             };
             chrome.runtime.sendMessage({type: 'decryptPassword', content: data}, (response) => {
