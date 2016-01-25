5c4172408a0550432c086987d757310e2fa842a0
Pavol Rusnak <stick@gk2.sk>
2016-01-25 19:54:38 +0100

    fix gitignore, bump version

--- release.sh
+++ release.sh
@@ -5,3 +5,5 @@ zip -r extension-newtab.zip extension/
 sed -i 's/(with New Tab feature)/(without New Tab feature)/' extension/manifest.json
 sed -i '/chrome_url_overrides/,+2d' extension/manifest.json
 zip -r extension-no-newtab.zip extension/
+# revert manifest
+git checkout extension/manifest.json
