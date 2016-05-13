ae0e8ac84c54a3b8fce4e469c3db666ffa05286d
chren <peter.privalinec@gmail.com>
2016-05-13 17:48:00 +0200

    reworked search with delay

--- source/app/dashboard/password_table/filter_input/filter_input.js
+++ source/app/dashboard/password_table/filter_input/filter_input.js
@@ -9,6 +9,7 @@
 
 var React = require('react'),
     NavDropdown = require('react-bootstrap').NavDropdown,
+    time = 0,
     FilterInput = React.createClass({
 
         getInitialState() {
@@ -22,15 +23,40 @@ var React = require('react'),
         },
 
         handleChange(event) {
-            this.setState({filter: event.target.value});
-            window.myStore.emit('filter', event.target.value);
+            this.stopTimer();
+            if (event.target.value.length == 0) {
+                this.clearFilter();
+            } else {
+                this.setState({filter: event.target.value});
+                this.startTimer();
+            }
         },
 
         submitForm(event) {
             event.preventDefault();
+            this.stopTimer();
+            window.myStore.emit('filter',  this.state.filter);
+        },
+
+        startTimer() {
+            this.timer = setInterval(this.tick, 100);
+        },
+
+        tick() {
+            time += 100;
+            if (time > 600) {
+                this.stopTimer();
+                window.myStore.emit('filter', this.state.filter);
+            }
+        },
+
+        stopTimer() {
+            clearInterval(this.timer);
+            time = 0;
         },
 
         clearFilter() {
+            this.stopTimer();
             this.setState({filter: ''});
             window.myStore.emit('filter', '');
         },
@@ -38,7 +64,8 @@ var React = require('react'),
         render(){
             return (
                 <span>
-                    <form role='filter' className={this.state.filter.length > 0 ? 'filter active' : 'filter'} onSubmit={this.submitForm}>
+                    <form role='filter' className={this.state.filter.length > 0 ? 'filter active' : 'filter'}
+                          onSubmit={this.submitForm}>
                         <input type='text' ref='filter' placeholder='Quick filter ...' value={this.state.filter}
                                onChange={this.handleChange} className='form-control'/>
                         <i className='icon ion-close-round clear' onClick={this.clearFilter}></i>
